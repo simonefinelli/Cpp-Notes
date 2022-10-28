81866207fe7ec44eddfed90cecd4001671949e92
Simone Finelli <simonefinelli.work@gmail.com>
2022-10-28 17:43:52 +0200

    Update ExceptionHandling section

---
+++ ExceptionHandling/06_ExceptionsInClasses/IllegalBalanceException.h
@@ -0,0 +1,14 @@
+#ifndef ILLEGALBALANCEEXCEPTION_H
+#define ILLEGALBALANCEEXCEPTION_H
+
+
+#include <exception>
+
+class IllegalBalanceException {
+public:
+    IllegalBalanceException() = default;
+    ~IllegalBalanceException() = default;
+};
+
+
+#endif // ILLEGALBALANCEEXCEPTION_H
